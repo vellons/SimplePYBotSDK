2e06488a90aa7f6a747a2047588c4fd58c6a5af7
vellons <vellons@users.noreply.github.com>
2021-02-09 20:31:04 +0100

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='simplepybotsdk',
+    version='0.1.0',
+    packages=['simplepybotsdk'],
+    install_requires=['pyramid==1.10.5'],
+    url='https://github.com/vellons/SimplePYBotSDK',
+    license='',
+    author='Vellons',
+    description='A simple Python3 library to manage the states of servomotors and sensor in a robot.'
+)
