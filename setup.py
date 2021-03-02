e41ff97064e84ce01cbe8af9f8c69c9c07215ddc
vellons <vellons@users.noreply.github.com>
2021-03-02 15:30:39 +0100

    Add License

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
 from setuptools import setup
 
-setup(
-    name='simplepybotsdk',
-    version='0.3.beta3',
-    packages=['simplepybotsdk'],
-    install_requires=['pyramid==1.10.5', 'websockets==8.1'],
-    python_requires='>=3.5',
-    url='https://github.com/vellons/SimplePYBotSDK',
-    license='',
-    author='Vellons',
-    description='A simple Python3 library to manage the states of servomotors and sensor in a robot.'
-)
+setup()
