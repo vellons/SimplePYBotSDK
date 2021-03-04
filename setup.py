3ab1f846e1c5a1e4d09d718d8fcd3230bb834d75
vellons <vellons@users.noreply.github.com>
2021-03-04 18:43:39 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 from setuptools import setup
+from simplepybotsdk import __version__ as version
 
-setup()
+setup(
+    name='simplepybotsdk',
+    version=version,
+    packages=['simplepybotsdk'],
+    install_requires=['pyramid==1.10.5', 'websockets==8.1'],
+    python_requires='>=3.5',
+)
