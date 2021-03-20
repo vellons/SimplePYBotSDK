c736cdb55c849869664e552c811730be38b6f103
vellons <vellons@users.noreply.github.com>
2021-03-20 21:29:47 +0100

    Version 0.5.0

--- simplepybotsdk/__init__.py
+++ simplepybotsdk/__init__.py
@@ -1,4 +1,5 @@
 from simplepybotsdk.configurations import VERSION as __version__
+from simplepybotsdk.configurations import LICENSE as __license__
 from simplepybotsdk.motor import Motor as Motor
 from simplepybotsdk.sensor import Sensor as Sensor
 from simplepybotsdk.robotSDK import RobotSDK as RobotSDK
